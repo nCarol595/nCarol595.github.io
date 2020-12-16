43212a8cc55fe00eb1343697f4b00b8c419b447b
Nydia Chaj <nchaj5@gmail.com>
2020-12-16 00:53:51 -0500

    more style changes

--- components/navbar.js
+++ components/navbar.js
@@ -5,11 +5,16 @@ import Link from 'next/link'
   
   const NavBar = () => (
     <div className={styles.navBar}>
-      <h2 className={utilStyles.navHead}>Nydia's Portfolio</h2>
-      <Link href="/posts/first-post">
-            <a className={utilStyles.navHead}>Projects</a>
-       </Link>
+      <h2 className={utilStyles.navHead}> -[.]- Nydia's Portfolio</h2>
+      
+       
     </div>
   );
   
   export default NavBar;
+  /**<Link href="/posts/first-post">
+            <a className={utilStyles.navHead2}>Contact</a>
+       </Link>
+       <Link href="/posts/first-post">
+            <a className={utilStyles.navHead2}>Project</a>
+       </Link> */
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,26 +8,42 @@ export default function FirstPost() {
     <Layout>
       
       <Head>
-        <title>First Post</title>
+        <title>Guinea Pig Enclosure</title>
       </Head>
+      <header className={newstyles.header}>
+        
+        <p className={newstyles.secstyle}>
+        <h2 className={newstyles.head2sec}>Guinea Pig Enclosure Project</h2>
+          <h3 className={newstyles.head3sec}>How it happened: </h3>A relative of mine approached me with the idea of
+          creating a guinea pig enclosure. Getting one custom made had to great of a price tag and 
+          so I decided why not give it a try.
+        
+          <h3 className={newstyles.head3sec}>Idea: </h3>The point was for the enclosure to have storage and for cleaning up to be
+          easier. Based on this and the desired length width and height of the enclosure I 
+          proceded with a basic sketch which I then presented to my relative. After some 
+          discussion I made a final sketch. 
       
-      <header className={newstyles.header2}>
-          <section className={newstyles.secstyle}>
+         <h3 className={newstyles.head3sec}>
+          Below is a in depth description of the build:
+         </h3>
+        </p>
+       
+        
+      </header>
+      <header className={newstyles.header2third}>
+          <p className={newstyles.secstyle}>
+              I made a basic diagram of the skeleton of the guinea pig cage enclosure:
+              [I need to insert an image here] 
               
-              [I need to insert an image here] : <h3 className={newstyles.header3}> A guinea pig cage</h3>
+                
+             
               
-                I need to write description here
-              
-          </section>
-          <section className={newstyles.secstyle}>
-            [Another image here]: <h3 className={newstyles.header3}>A table</h3>
-            I need to write description here
-            
-          </section>
+          </p>
+          
       </header>
       <section className={newstyles.abme}> 
           <p>
-              [DELETE]
+              
           </p>
       </section>
     </Layout>
