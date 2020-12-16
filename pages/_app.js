bc16142fe21ec160a909bce79aeebfc98bce9231
Nydia Chaj <nchaj5@gmail.com>
2020-12-15 20:25:19 -0500

    style changes and _document.js file added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
-import '../styles/global.css'
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+import '../styles/global.css'
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+export default MyApp
+// This default export is required in a new `pages/_app.js` file.
+//export default function MyApp({ Component, pageProps }) {
+//  return <Component {...pageProps} />
+//}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import NavBar from "../components/navbar"
-import Link from 'next/link'
-import styles from '../components/layout.module.css'
+
 
 export default function Home() {
   return (
